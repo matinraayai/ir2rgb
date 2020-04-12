0395137ffd8e7f3161b6d25de67c7bac3f0a7ffb
Matin Raayai <raayaiardakani.m@husky.neu.edu>
2020-04-12 14:46:54 -0400

    Modified options.

--- options/base_options.py
+++ options/base_options.py
@@ -4,41 +4,77 @@ from util import util
 import torch
 
 class BaseOptions():
+    """
+    Base options for training.
+    """
     def __init__(self):
         self.parser = argparse.ArgumentParser()
         self.initialized = False
 
     def initialize(self):                
-        self.parser.add_argument('--dataroot', type=str, default='datasets/KAIST/')        
-        self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
-        self.parser.add_argument('--loadSize', type=int, default=512, help='scale images to this size')
-        self.parser.add_argument('--fineSize', type=int, default=512, help='then crop to this size')
-        self.parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels')
-        self.parser.add_argument('--label_nc', type=int, default=0, help='number of labels')        
-        self.parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels')        
+        self.parser.add_argument('--dataroot', type=str, 
+                                 default='kaist-rgbt/images/')        
+        self.parser.add_argument('--batchSize', type=int, 
+                                 default=1, help='input batch size')
+        self.parser.add_argument('--loadSize', type=int, 
+                                 default=512, help='scale images to this size')
+        self.parser.add_argument('--fineSize', type=int, 
+                                 default=512, help='then crop to this size')
+        self.parser.add_argument('--input_nc', type=int, 
+                                 default=3, help='# of input image channels')
+        self.parser.add_argument('--label_nc', type=int, 
+                                 default=0, help='number of labels')        
+        self.parser.add_argument('--output_nc', type=int, 
+                                 default=3, help='# of output image channels')        
 
         # network arch
-        self.parser.add_argument('--netG', type=str, default='composite', help='selects model to use for netG')        
-        self.parser.add_argument('--ngf', type=int, default=128, help='# of gen filters in first conv layer')
-        self.parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')    
-        self.parser.add_argument('--n_blocks', type=int, default=9, help='number of resnet blocks in generator')
-        self.parser.add_argument('--n_downsample_G', type=int, default=3, help='number of downsampling layers in netG')        
+        self.parser.add_argument('--netG', type=str, 
+                                 default='composite', help='selects model to use for netG')        
+        self.parser.add_argument('--ngf', type=int, 
+                                 default=128, help='# of gen filters in first conv layer')
+        self.parser.add_argument('--ndf', type=int, 
+                                 default=64, help='# of discrim filters in first conv layer')    
+        self.parser.add_argument('--n_blocks', type=int, 
+                                 default=9, help='number of resnet blocks in generator')
+        self.parser.add_argument('--n_downsample_G', type=int, 
+                                 default=3, help='number of downsampling layers in netG')        
 
-        self.parser.add_argument('--gpu_ids', type=str, default='0,1,2,3', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        self.parser.add_argument('--n_gpus_gen', type=int, default=-1, help='how many gpus are used for generator (the rest are used for discriminator). -1 means use all gpus')
-        self.parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
-        self.parser.add_argument('--dataset_mode', type=str, default='temporal', help='chooses how datasets are loaded. [unaligned | aligned | single]')
-        self.parser.add_argument('--model', type=str, default='vid2vid', help='chooses which model to use. vid2vid, test')        
-        self.parser.add_argument('--nThreads', default=16, type=int, help='# threads for loading data')
-        self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
-        self.parser.add_argument('--norm', type=str, default='batch', help='instance normalization or batch normalization')
-        self.parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
-        self.parser.add_argument('--display_winsize', type=int, default=512,  help='display window size')
-        self.parser.add_argument('--display_id', type=int, default=0, help='window id of the web display')        
-        self.parser.add_argument('--tf_log', action='store_true', help='if specified, use tensorboard logging. Requires tensorflow installed')
+        self.parser.add_argument('--gpu_ids', type=str, default='0', 
+                                 help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        self.parser.add_argument('--n_gpus_gen', type=int, 
+                                 default=-1, 
+                                 help='how many gpus are used for generator'
+                                 '(the rest are used for discriminator). -1 means use all gpus')
+        self.parser.add_argument('--name', type=str, 
+                                 default='experiment_name', 
+                                 help='name of the experiment. It decides where'
+                                 'to store samples and models')
+        self.parser.add_argument('--dataset_mode', type=str, default='temporal', 
+                                 help='chooses how datasets are loaded. [unaligned | aligned | single]')
+        self.parser.add_argument('--model', type=str, default='vid2vid', 
+                                 help='chooses which model to use. vid2vid, test')        
+        self.parser.add_argument('--nThreads', default=8, type=int, 
+                                 help='# threads for loading data')
+        self.parser.add_argument('--checkpoints_dir', type=str, 
+                                 default='./checkpoints', help='models are saved here')
+        self.parser.add_argument('--norm', type=str, default='batch', 
+                                 help='instance normalization or batch normalization')
+        self.parser.add_argument('--serial_batches', action='store_true', 
+                                 help='if true, takes images in order to make batches, otherwise takes them randomly')
+        self.parser.add_argument('--display_winsize', type=int, default=512,  
+                                 help='display window size')
+        self.parser.add_argument('--display_id', type=int, default=0, 
+                                 help='window id of the web display')        
+        self.parser.add_argument('--tf_log', action='store_true', 
+                                 help='if specified, use tensorboard logging. Requires tensorflow installed')
                         
-        self.parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
-        self.parser.add_argument('--resize_or_crop', type=str, default='scaleWidth', help='scaling and cropping of images at load time [resize_and_crop|crop|scaledCrop|scaleWidth|scaleWidth_and_crop|scaleWidth_and_scaledCrop|scaleHeight|scaleHeight_and_crop] etc')
+        self.parser.add_argument('--max_dataset_size', type=int, 
+                                 default=float(50), 
+                                 help='Maximum number of samples allowed per dataset.'
+                                 'If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+        self.parser.add_argument('--resize_or_crop', type=str, default='scaleWidth', 
+                                 help='scaling and cropping of images at load time '
+                                 '[resize_and_crop|crop|scaledCrop|scaleWidth|scaleWidth_and_crop|scaleWidth_and_scaledCrop|scaleHeight|scaleHeight_and_crop] etc')
         self.parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data argumentation')                    
     
         # more features as input        
@@ -77,10 +113,14 @@ class BaseOptions():
         self.parser.add_argument('--basic_point_only', action='store_true', help='only use basic joint keypoints for openpose, without hand or face keypoints')
         
         # miscellaneous                
-        self.parser.add_argument('--load_pretrain', type=str, default='', help='if specified, load the pretrained model')                
-        self.parser.add_argument('--debug', action='store_true', help='if specified, use small dataset for debug')
-        self.parser.add_argument('--fp16', action='store_true', default=False, help='train with AMP')
-        self.parser.add_argument('--local_rank', type=int, default=0, help='local rank for distributed training')
+        self.parser.add_argument('--load_pretrain', type=str, default='', 
+                                 help='if specified, load the pretrained model')                
+        self.parser.add_argument('--debug', action='store_true', 
+                                 help='if specified, use small dataset for debug')
+        self.parser.add_argument('--fp16', action='store_true', 
+                                 default=False, help='train with AMP')
+        self.parser.add_argument('--local_rank', type=int, 
+                                 default=0, help='local rank for distributed training')
 
         self.initialized = True
 
