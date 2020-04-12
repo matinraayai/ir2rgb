9b515bcfdc1f44a1ec690835dbc259891b6cf115
steveschmidt95-neu <schmidt.st@husky.neu.edu>
2020-04-12 17:51:20 -0400

    fixed 2 bugs

--- data/rcnn_dataset.py
+++ data/rcnn_dataset.py
@@ -13,11 +13,16 @@ class RCNNDataset(object):
         self.root = opt.dataroot
         self.dir_B = os.path.join(opt.dataroot, opt.phase + '_B')
         self.B_paths = sorted(make_grouped_dataset(self.dir_B))
+        self.dir_annotations = os.path.join(opt.dataroot, opt.phase + '_annotations')
+        self.box_baths =sorted(make_grouped_dataset(self.dir_annotations))
+        
         check_path_valid(self.B_paths)
+        check_path_valid(self.dir_annotations)
+        
         if opt.use_instance:                
             self.dir_inst = os.path.join(opt.dataroot, opt.phase + '_inst')
             self.I_paths = sorted(make_grouped_dataset(self.dir_inst))
-            check_path_valid(self.A_paths, self.I_paths)
+            check_path_valid(self.B_paths, self.I_paths)
 
         self.n_of_seqs = len(self.B_paths)                 # number of sequences to train       
         self.seq_len_max = max([len(B) for B in self.B_paths])        
@@ -27,10 +32,9 @@ class RCNNDataset(object):
     def __getitem__(self, index):
         # load images and masks
         tG = self.opt.n_frames_G
-        B_paths = self.B_paths[index % self.n_of_seqs]                3
+        B_paths = self.B_paths[index % self.n_of_seqs]                
         if self.opt.use_instance:
             I_paths = self.I_paths[index % self.n_of_seqs] 
-            
             
         # setting parameters
         n_frames_total, start_idx, t_step = get_video_params(self.opt, self.n_frames_total, len(B_paths), index)     
@@ -41,7 +45,7 @@ class RCNNDataset(object):
         transform_scaleB = get_transform(self.opt, params)                
         
         # read in images
-        A = B = inst = 0
+        B = inst = 0
         for i in range(n_frames_total):            
             B_path = B_paths[start_idx + i * t_step]                    
             Bi = self.get_image(B_path, transform_scaleB)
@@ -53,7 +57,7 @@ class RCNNDataset(object):
                 Ii = self.get_image(I_path, transform_scaleB) * 255.0
                 inst = Ii if i == 0 else torch.cat([inst, Ii], dim=0)                
 
-        return_list = {'A': A, 'B': B, 'inst': inst, , 'B_paths': B_path}
+        return_list = {'B': B, 'inst': inst, 'B_paths': B_path}
         return return_list
         
         
