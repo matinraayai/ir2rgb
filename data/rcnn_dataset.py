378d743ff29f82e95c0397f9e0be381fafdaafa7
Steve Schmidt <schmidt.st@husky.neu.edu>
2020-04-12 17:36:27 -0400

    Starting on RCNN training scripts

--- data/rcnn_dataset.py
+++ data/rcnn_dataset.py
@@ -2,51 +2,65 @@ import os
 import numpy as np
 import torch
 from PIL import Image
+from data.image_folder import make_grouped_dataset, check_path_valid
+import torch
+from data.base_dataset import BaseDataset, get_img_params, get_transform, get_video_params
 
 
 class RCNNDataset(object):
-    def __init__(self, root):
-        self.root = root
-        # load all image files, sorting them to
-        # ensure that they are aligned
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "/images"))))
-        self.annots = list(sorted(os.listdir(os.path.join(root, "/annotations"))))
+    def __init__(self, opt):
+        self.opt = opt
+        self.root = opt.dataroot
+        self.dir_B = os.path.join(opt.dataroot, opt.phase + '_B')
+        self.B_paths = sorted(make_grouped_dataset(self.dir_B))
+        check_path_valid(self.B_paths)
+        if opt.use_instance:                
+            self.dir_inst = os.path.join(opt.dataroot, opt.phase + '_inst')
+            self.I_paths = sorted(make_grouped_dataset(self.dir_inst))
+            check_path_valid(self.A_paths, self.I_paths)
 
-    def __getitem__(self, idx):
-        # load images ad masks
-        img_path = os.path.join(self.root, "", self.imgs[idx])
-        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
-        img = Image.open(img_path).convert("RGB")
+        self.n_of_seqs = len(self.B_paths)                 # number of sequences to train       
+        self.seq_len_max = max([len(B) for B in self.B_paths])        
+        self.n_frames_total = self.opt.n_frames_total      # current number of frames to train in a single iteration
+        
 
-        # get bounding box coordinates for each mask
-        num_objs = len(obj_ids)
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
+    def __getitem__(self, index):
+        # load images and masks
+        tG = self.opt.n_frames_G
+        B_paths = self.B_paths[index % self.n_of_seqs]                3
+        if self.opt.use_instance:
+            I_paths = self.I_paths[index % self.n_of_seqs] 
+            
+            
+        # setting parameters
+        n_frames_total, start_idx, t_step = get_video_params(self.opt, self.n_frames_total, len(B_paths), index)     
 
-        # convert everything into a torch.Tensor
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
-        masks = torch.as_tensor(masks, dtype=torch.uint8)
+        # setting transformers
+        B_img = Image.open(B_paths[start_idx]).convert('RGB')        
+        params = get_img_params(self.opt, B_img.size)          
+        transform_scaleB = get_transform(self.opt, params)                
+        
+        # read in images
+        A = B = inst = 0
+        for i in range(n_frames_total):            
+            B_path = B_paths[start_idx + i * t_step]                    
+            Bi = self.get_image(B_path, transform_scaleB)
+                      
+            B = Bi if i == 0 else torch.cat([B, Bi], dim=0)            
 
-        image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+            if self.opt.use_instance:
+                I_path = I_paths[start_idx + i * t_step]                
+                Ii = self.get_image(I_path, transform_scaleB) * 255.0
+                inst = Ii if i == 0 else torch.cat([inst, Ii], dim=0)                
 
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["image_id"] = image_id
-
-        return img, target
+        return_list = {'A': A, 'B': B, 'inst': inst, , 'B_paths': B_path}
+        return return_list
+        
+        
+       
 
     def __len__(self):
         return len(self.imgs)
+
+datat = RCNNDataset('')
 
--- train_rcnn.py
+++ train_rcnn.py
@@ -1,1 +1,21 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from data.rcnn_dataset import RCNNDataset
+from engine import train_one_epoch, evaluate
+import utils
+import torch
 
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+dataset = RCNNDataset()
+data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn)
+
+# For Training
+images,targets = next(iter(data_loader))
+images = list(image for image in images)
+targets = [{k: v for k, v in t.items()} for t in targets]
+output = model(images,targets)   # Returns losses and detections
+# For inference
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)           # Returns predictions
