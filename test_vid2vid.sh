606b8873aa1652b856daeab8cf1a733b12dc4871
matinraayai <raayaiardakani.m@husky.neu.edu>
2020-04-17 11:47:24 -0400

    Copied the new scripts for train and test

---
+++ test_vid2vid.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python test.py --name third_run --input_nc 3 --output_nc 3 --use_real_img --label_feat
---
+++ train_vid2vidRCNN.sh
@@ -0,0 +1,1 @@
+python train_vid2vid.py --name test_run --label_feat --input_nc 3 --output_nc 3 --tf_log
